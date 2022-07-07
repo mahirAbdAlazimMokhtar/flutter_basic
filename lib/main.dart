33e490a727b40df26a5b8aad7f02227bb9cf35c1
Mahir Molhtar <mahr.bwb@gmail.com>
2022-07-07 18:42:34 +0300

    buttons in flutter with icon without icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basic/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(HomePage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) ;
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-   return MaterialApp(
-     home: Scaffold(
-       body: HomePage(),
-     ),
-   );
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OutlinedButton(
+                onPressed: () {},
+                child: const Text('this is Button'),
+              ),
+              OutlinedButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.access_alarm_rounded),
+                label: const Text('this is Button'),
+              ),
+              ElevatedButton(onPressed: (){}, child: const Text('this is ElevatedButton')),
+              ElevatedButton.icon(onPressed: (){}, icon: Icon(Icons.media_bluetooth_off),label: const Text('this is ElevatedButton'),),
+              TextButton.icon(onPressed: (){}, icon: Icon(Icons.media_bluetooth_off),label: const Text('this is ElevatedButton'),),
+              TextButton(onPressed: (){}, child: const Text('this is ElevatedButton'))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
-
