7f48b5b0b6d81f33adadec841136d122c913848b
Mahir Molhtar <mahr.bwb@gmail.com>
2022-07-12 00:09:52 +0300

    show and hide password and toggle between icon visible and off

---
+++ lib/textField.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatefulWidget {
+  const CustomTextField({Key? key}) : super(key: key);
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+bool passwordShow = true;
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        color: Colors.grey.shade100,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              margin: const EdgeInsets.all(20),
+              child: TextField(
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: passwordShow,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  hintStyle: const TextStyle(
+                    color: Colors.purple,
+
+                    //backgroundColor: Colors.blueAccent,
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        passwordShow = !passwordShow;
+                      });
+                    },
+                    //this for toggle between icon show password and hide password
+                    icon:passwordShow ?  Icon(Icons.visibility) : Icon(Icons.visibility_off),
+                  ),
+                  // labelText: 'Enter your Email',
+                  // labelStyle: TextStyle(
+                  //   color: Colors.purple,
+                  //   //backgroundColor: Colors.blueAccent,
+                  // ),
+                  label: const Text('this a label'),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
